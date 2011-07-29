70e34b8ce86d3e9b751494072370982bfcc5fe29
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-07-29 07:11:50 +0000

    separating out payment_NZ_gateways
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_securatech/trunk@1386 950b781c-c544-efab-2f99-90083ee69683

---
+++ _config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author nicolaas [at] www.sunnysideup.co.nz
+**/
+
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START payment_NZ_gateways MODULE ----------------===================
+//SecurePayTechPayment::set_spt_merchant_id("TESTDIGISPL1")
+//SecurePayTechPayment::set_spt_merchant_key("abc");
+//===================---------------- END payment_NZ_gateways MODULE ----------------===================
